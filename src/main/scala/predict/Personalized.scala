9f82596e994f5d3c5573c885405f55a0d8d54296
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2022-03-07 17:24:16 +0100

    Personalised first implementation

--- src/main/scala/predict/Personalized.scala
+++ src/main/scala/predict/Personalized.scala
@@ -11,6 +11,9 @@ import org.apache.log4j.Level
 import scala.math
 import shared.predictions._
 
+// custom import
+import predict.Baseline._
+
 
 class PersonalizedConf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val train = opt[String](required = true)
@@ -59,13 +62,13 @@ object Personalized extends App {
           "3.Measurements" -> ujson.Num(conf.num_measurements())
         ),
         "P.1" -> ujson.Obj(
-          "1.PredUser1Item1" -> ujson.Num(0.0), // Prediction of item 1 for user 1 (similarity 1 between users)
-          "2.OnesMAE" -> ujson.Num(0.0)         // MAE when using similarities of 1 between all users
+          "1.PredUser1Item1" -> ujson.Num(predictor(train)(simOnes)(1,1)), // Prediction of item 1 for user 1 (similarity 1 between users)
+          "2.OnesMAE" -> ujson.Num(MAE(test, predictor(train)(simOnes)))         // MAE when using similarities of 1 between all users
         ),
         "P.2" -> ujson.Obj(
-          "1.AdjustedCosineUser1User2" -> ujson.Num(0.0), // Similarity between user 1 and user 2 (adjusted Cosine)
-          "2.PredUser1Item1" -> ujson.Num(0.0),  // Prediction item 1 for user 1 (adjusted cosine)
-          "3.AdjustedCosineMAE" -> ujson.Num(0.0) // MAE when using adjusted cosine similarity
+          "1.AdjustedCosineUser1User2" -> ujson.Num(adjustedCosine(train)(1,2)), // Similarity between user 1 and user 2 (adjusted Cosine)
+          "2.PredUser1Item1" -> ujson.Num(predictor(train)(adjustedCosine(train))(1,1)),  // Prediction item 1 for user 1 (adjusted cosine)
+          "3.AdjustedCosineMAE" -> ujson.Num(MAE(test, predictor(train)(adjustedCosine(train)))) // MAE when using adjusted cosine similarity
         ),
         "P.3" -> ujson.Obj(
           "1.JaccardUser1User2" -> ujson.Num(0.0), // Similarity between user 1 and user 2 (jaccard similarity)
@@ -82,4 +85,105 @@ object Personalized extends App {
 
   println("")
   spark.close()
+
+  def MAE(data: Seq[Rating], predict: (Int, Int)=> Double): Double = {
+    val acc = data.foldLeft((0.0,0)){
+      (acc, x)=> {
+        (acc._1+(predict(x.user, x.item)-x.rating).abs, acc._2+1)
+      }
+    }
+    acc._1/acc._2
+  }
+
+  def simOnes = (user1: Int, user2: Int)=> 1.0
+
+  def predictor(train: Seq[Rating])(sim: ((Int, Int)=> Double)): (Int, Int)=> Double = {
+    // Compute necessary values for the predictions
+    val ratingsByItems = ratingByItems(train)
+    val usAvgs = computeAllUsersAvg(train)
+    lazy val globalAvgValue = globalAvg(train)
+    
+    (user: Int, item: Int)=> {
+      // user that rated this item 
+      val users = ratingsByItems.getOrElse(item, Nil)
+      // get average rating value for this user or the global average if the user didn't rate anything in the train set
+      val avg = usAvgs.getOrElse(user, globalAvgValue)
+
+      // get similarity with current user and the rating 
+      val simVal = users.map(x=> (x.rating, sim(user, x.user)))
+
+      val sumSim = simVal.foldLeft((0.0, 0.0)){
+        (acc, x)=>{
+          (acc._1 + x._1*x._2, acc._2 + x._2.abs)
+        }
+      }
+      // compute the prediction
+      val denomSum = if (sumSim._2!=0) sumSim._1/sumSim._2 else 0.0
+      avg + denomSum* scale(avg+ denomSum, avg)
+    }
+  }
+
+  def groupBy(data:Seq[Rating])(key: Rating => Int): Map[Int, Seq[Rating]] = 
+    data.foldLeft(Map[Int, Seq[Rating]]()){
+      // The accumulator is a map mapping the key (an int) to a pair (Double, Int) corresponding to the running sum of the value we want to compute and the number of computed value respectively 
+      (acc, x)=>{
+        // We access the value already stored or get 0 if no value was stored for this key
+        val cur:Seq[Rating] = acc.getOrElse(key(x), Seq[Rating]())
+        // Update of the map
+        acc + (key(x) -> (x+:cur))
+      }
+    }
+
+  def ratingByUsers(data: Seq[Rating]): Map[Int, Seq[Rating]] = groupBy(data)(_.user)
+  
+  def ratingByItems(data: Seq[Rating]): Map[Int, Seq[Rating]] = groupBy(data)(_.item)
+
+  def adjustedCosine(train:Seq[Rating]): (Int, Int)=> Double = {
+    val usAvg = computeAllUsersAvg(train)
+    lazy val globalAvgValue = globalAvg(train)
+
+    val dev = (x: Rating) => {
+      val avg = usAvg.getOrElse(x.user, globalAvgValue)
+      (x.rating-avg)/scale(x.rating, avg)
+    }
+
+    val mapped = train.map(x=>(x.user, dev(x)))
+
+    val normByUsers = mapped.foldLeft(Map[Int,Double]()){(acc,x) =>
+       val cur:Double = acc.getOrElse(x._1, 0.0)
+        // Update of the map
+        acc + (x._1 -> (cur+x._2*x._2))
+    }
+
+    val itemsByU = ratingByUsers(train)
+
+    (user1: Int, user2: Int)=> {
+      val ratings1 = itemsByU.getOrElse(user1, Nil)
+      val ratings2 = itemsByU.getOrElse(user2, Nil)
+      if(ratings1.length==0 || ratings2.length==0) 0.0
+      else{
+        val items1 = ratings1.map(_.item)
+        
+        val items2 = ratings2.map(_.item)
+        val inter = items1.toSet.intersect(items2.toSet)
+
+        val remaining2 = ratings2.foldLeft(Map[Int, Double]()){
+          (acc, x)=>{
+            if(inter.contains(x.item)){ acc + (x.item -> (dev(x)/normByUsers(user2)))}
+            else acc
+          }
+        }
+
+        ratings1.foldLeft(0.0){
+          (acc, x)=>{
+            if(inter.contains(x.item)){
+              val norm = normByUsers.getOrElse(user1, 0.0)
+              if(normByUsers==0) acc
+              else acc + dev(x) / norm *remaining2(x.item)
+            } else acc
+          }
+        }
+      }
+    }
+  }
 }
