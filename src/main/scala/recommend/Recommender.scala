d7d7955a4d5790b38349e3b2b0b3b03facedbe32
EloDoyard <eloise.doyard@epfl.ch>
2022-03-19 12:49:51 +0100

    null pointer exception in recommender

--- src/main/scala/recommend/Recommender.scala
+++ src/main/scala/recommend/Recommender.scala
@@ -9,6 +9,10 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
 import shared.predictions._
+import scala.util.Sorting
+import predict.kNN._
+import predict.Baseline._
+import predict.Personalized._
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val data = opt[String](required = true)
@@ -65,19 +69,20 @@ object Recommender extends App {
   conf.json.toOption match {
     case None => ; 
     case Some(jsonFile) => {
+      val augmented = data.union(personal)
       val answers = ujson.Obj(
         "Meta" -> ujson.Obj(
           "data" -> conf.data(),
           "personal" -> conf.personal()
         ),
         "R.1" -> ujson.Obj(
-          "PredUser1Item1" -> ujson.Num(0.0) // Prediction for user 1 of item 1
+          "PredUser1Item1" -> ujson.Num(predictKNN(augmented, 300)(1,1)) //0.0) // Prediction for user 1 of item 1
         ),
           // IMPORTANT: To break ties and ensure reproducibility of results,
           // please report the top-3 recommendations that have the smallest
           // movie identifier.
 
-        "R.2" -> List((254, 0.0), (338, 0.0), (615, 0.0)).map(x => ujson.Arr(x._1, movieNames(x._1), x._2))
+        "R.2" -> recommendations(augmented, 300)(944, 3).map(x => ujson.Arr(x._1, movieNames(x._1), x._2))
        )
       val json = write(answers, 4)
 
@@ -86,6 +91,35 @@ object Recommender extends App {
       printToFile(json, jsonFile)
     }
   }
+
+  // implicit object PredictionOrd extends math.Ordering[(Int, Double)] {
+  //   def compare (x:(Int, Double), y:(Int, Double)):Int = {
+  //     if (x._2==y._2) {
+  //       1 if x._1<=y._1 else -1
+  //     } else {
+  //       1 if x._2<y._2 else -1
+  //     }
+  //   }
+  // }
+
+  def recommendations (ratings : Seq[Rating], k : Int) : (Int, Int) => Seq[(Int, Double)] = {
+    val knn = predictKNN(data, k)
+
+    (user : Int, n : Int) => {
+      // val notRated = data.map(_.item).toSet-rated.map(_.item).toSet
+
+      val predictions = ratings.filter(x=> x.user != user).map(_.item).distinct.map(x=> (x, knn(user, x)))
+
+      Sorting.stableSort(predictions, (x:(Int, Double),y:(Int, Double)) => x._2>y._2)
+      Sorting.stableSort(predictions, (x:(Int, Double),y:(Int, Double))=> x._1<y._1)
+      // Sorting.quickSort(predictions)(PredictionOrd).take(n)
+      // Sorting.quickSort(predictions)(Ordering[(Int, Double)].on(x=> (x._2, x._1)))
+      predictions.take(n)
+    }
+    
+
+  }
+
 
   println("")
   spark.close()
